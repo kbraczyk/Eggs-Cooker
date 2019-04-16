6d07b6408c46c6a9f1f9a5e5d9ee130743983dd3
kbraczyk <braczykkrzysztof@gmail.com>
2019-04-16 12:36:33 +0200

    Add button stop and reset

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const radios = [...document.querySelectorAll(".cookingDegree")];
-const btn = document.querySelector(".timer button");
+const startBtn = document.querySelector(".timer .start");
+const stopBtn = document.querySelector(".timer .stop");
+const resetBtn = document.querySelector(".timer .reset");
 const timeControl = document.querySelector(".timer input")
+
 
 let hours = 0;
 let minutes = 0;
@@ -14,11 +17,13 @@ timeControl.value = showTime();
 
 radios.forEach((radio) => {
     radio.addEventListener("click", () => {
+        secends = 0;
         minutes = (1 * radio.value);
         timeControl.value = showTime();
         timeControl.style.color = "white";
     })
 })
+let timerX;
 
 function time() {
     if (secends > 0) {
@@ -36,16 +41,38 @@ function time() {
     if (hours == 0 && minutes == 0 && secends == 0) {
         timeControl.style.color = "red";
         timeControl.value = `Eggs is done`;
-        btn.disabled = false;
         radios.forEach((radio) => radio.disabled = false);
         return;
     }
-    setTimeout(time, 1000);
+    timerX = setTimeout(time, 1000);
 };
 
-btn.addEventListener("click", (e) => {
+startBtn.addEventListener("click", (e) => {
     e.preventDefault();
     time();
-    btn.disabled = true;
+    startBtn.style.display = "none";
+    stopBtn.style.display = "block";
     radios.forEach((radio) => radio.disabled = true);
+})
+
+resetBtn.addEventListener("click", () => {
+    if (confirm('Countdown will be reset, do you want to continue?')) {
+        startBtn.style.display = "block";
+        stopBtn.style.display = "none";
+        radios.forEach((radio) => {
+            radio.disabled = false;
+            radio.checked = false;
+        });
+        clearInterval(timerX);
+        minutes = 0;
+        secends = 0;
+        timeControl.value = showTime();
+    }
+})
+
+stopBtn.addEventListener("click", () => {
+    startBtn.style.display = "block";
+    stopBtn.style.display = "none";
+    radios.forEach((radio) => radio.disabled = false);
+    clearInterval(timerX);
 })
